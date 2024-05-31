7a3e4e43fad849096cdc6576ddead064f9043d11
Alina Linscheidt <a@linscheidt.org>
2024-05-31 17:04:36 +0200

    Prompting variables

--- api/api.py
+++ api/api.py
@@ -14,6 +14,15 @@ openai_api_key = os.environ["OPENAI_API_KEY"]
 # System Prompt for the chatbot
 system_prompt = os.environ ["SYSTEM_PROMPT"]
 
+# Care tips prompt
+care_tips_prompt_system = os.environ ["CARE_TIPS_PROMPT_SYSTEM"]
+ 
+ # Care tips prompt
+care_tips_prompt_user = os.environ ["CARE_TIPS_PROMPT_USER"]
+
+# Variable if not recognized
+generic_plant = os.environ ["GENERIC_PLANT_NAME"]
+
 # Initialize the OpenAI API client
 openai.api_key = openai_api_key
 
@@ -32,7 +41,7 @@ CLIENT = InferenceHTTPClient(
 def inference_local_image(image_path: str):
     result = CLIENT.infer(image_path, model_id="houseplants-image-detection/1")
     # Extract plant name from the result (assumption)
-    plant_name = result['predictions'][0]['class']
+    plant_name = result['predictions'][0]['class'] if result['predictions'] else generic_plant
     return plant_name
 
 # Function to get care tips using OpenAI GPT-3
@@ -40,15 +49,14 @@ def get_care_tips(plant_class):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo-16k",
                 messages=[
-            {"role": "system", "content": f"You are in the role of a plant expert. You are witty and wise. You give care tips for a {plant_class}. Always mention how much or how often the plant needs to be watered and how much sunlight it needs. Mention special soil/fertilizer if necessary."},
-            {"role": "user", "content": f"Give some care tips for a {plant_class}. Use no more than 200 words."}
+            {"role": "system", "content": care_tips_prompt_system.format(plant_class = plant_class)},
+            {"role": "user", "content": care_tips_prompt_user.format(plant_class = plant_class)},
         ],
         temperature=0.7,
-        max_tokens=100,
+        max_tokens=1000,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
-        stop=["\n"]
     )
     return completion.choices[0].message.content
 
@@ -61,11 +69,10 @@ def handle_user_question(plant_class, user_question):
             {"role": "user", "content": f"{user_question}"}
         ],
         temperature=0.7,
-        max_tokens=100,
+        max_tokens=1000,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
-        stop=["\n"]
     )
     return completion.choices[0].message.content
 
@@ -77,6 +84,9 @@ async def prediction(file: UploadFile = File(...)):
         f.write(file.file.read())
 
     plant = inference_local_image(file_location)
+    if plant=="generic plant":
+        return {"text": "No care tips available, follow your heart. 💚", "plant": plant}
+    
     care_tips = get_care_tips(plant)
     return {"text": care_tips, "plant": plant}  # Returns the care tips as a JSON response.
 
